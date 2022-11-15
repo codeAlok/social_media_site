2b78c110e0a45f40c4bbf2b4b2913405d074b7f1
alok <alok60541@gmail.com>
2022-11-16 01:28:36 +0530

    custom theme color

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root'); // to change variable value of :root css
-
+const colorPalette = document.querySelectorAll('.choose-color span');
 
 // ***** remove active class from all menu items ****
 const changeActiveItem = () => {
@@ -136,6 +136,45 @@ fontSizes.forEach(size => {
     });
  
 });
+
+
+// ================ Change primary colors ==================
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primaryHue;
+
+        changeActiveColorClass();
+
+        if(color.classList.contains('color-1')) {
+            primaryHue = 252;
+        }
+        else if(color.classList.contains('color-2')) {
+            primaryHue = 52;
+        }
+        else if(color.classList.contains('color-3')) {
+            primaryHue = 352;
+        }
+        else if(color.classList.contains('color-4')) {
+            primaryHue = 152;
+        }
+        else if(color.classList.contains('color-5')) {
+            primaryHue = 202;
+        }
+
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    })
+})
+
 
 
 // ************* Add on/ Improvement / Fixes to do Later **************
