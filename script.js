faa02f84cd179489df5dbd1c0fd5b81196fc9cbc
alok <alok60541@gmail.com>
2022-11-16 02:28:17 +0530

    bg theme color add

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root'); // to change variable value of :root css
 const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 // ***** remove active class from all menu items ****
 const changeActiveItem = () => {
@@ -174,6 +177,65 @@ colorPalette.forEach(color => {
         root.style.setProperty('--primary-color-hue', primaryHue);
     })
 })
+
+
+// ================ Background Theme color =================
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+// Changes background color
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+Bg1.addEventListener('click', () => {
+    // add active class
+    Bg1.classList.add('active');
+
+    // remove active class from the others
+    Bg2. classList.remove('active');
+    Bg3.classList.remove('active');
+
+    // remove customized changes from local storage
+    window.location.reload();
+
+});
+
+Bg2.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    // add active class
+    Bg2.classList.add('active');
+
+    // remove active class from the others
+    Bg1. classList.remove('active');
+    Bg3.classList.remove('active');
+
+    changeBG();
+});
+
+Bg3.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    // add active class
+    Bg3.classList.add('active');
+
+    // remove active class from the others
+    Bg1. classList.remove('active');
+    Bg2.classList.remove('active');
+
+    changeBG();
+});
+
+
+
 
 
 
