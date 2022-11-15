585ac8bc444e8b4ecc08d003747baa248d9d3635
alok <alok60541@gmail.com>
2022-11-16 01:06:13 +0530

    custom font-size logic

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
 
+// *** Theme ***
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root'); // to change variable value of :root css
+
+
 // ***** remove active class from all menu items ****
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -66,6 +73,69 @@ messagesNotification.addEventListener('click', () => {
 })
 
 
+// ======== Theme and Display Customization ========
+
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')) {
+        themeModal.style.display = 'none';
+    }
+}
+
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click', openThemeModal);
+
+
+// =============== Font Sizes ================
+
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    });
+};
+
+fontSizes.forEach(size => {
+
+    size.addEventListener('click', () => {
+        removeSizeSelector(); 
+
+        let fontSize;
+        size.classList.toggle('active');
+
+        if(size.classList.contains('font-size-1')) {
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '5.4rem');
+        }
+        else if(size.classList.contains('font-size-2')) {
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        }
+        else if(size.classList.contains('font-size-3')) {
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        }
+        else if(size.classList.contains('font-size-4')) {
+            fontSize = '19px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rem');
+        }
+        else if(size.classList.contains('font-size-5')) {
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-10rem');
+            root.style.setProperty('--sticky-top-right', '-33rem');
+        }
+
+        // change font size of the Root HTML element
+        document.querySelector('html').style.fontSize = fontSize;
+    });
+ 
+});
 
 
 // ************* Add on/ Improvement / Fixes to do Later **************
